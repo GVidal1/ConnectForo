7f99ffefb78972b16c72c6725faf334ea77529ff
Riskoder <g.vidal1g+1@gmail.com>
2025-04-29 18:27:56 -0400

    Implementacion inicial del microservicio de categorias

---
+++ microservicios/microservicios-connectforo/microservicio-categorias/src/main/java/com/microservicio/categorias/microservicio_categorias/MicroservicioCategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.categorias.microservicio_categorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCategoriasApplication.class, args);
+	}
+
+}
---
+++ microservicios/microservicios-connectforo/microservicio-categorias/src/test/java/com/microservicio/categorias/microservicio_categorias/MicroservicioCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.categorias.microservicio_categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
