0641329c31c23069f81ebd2c1366305dc2506481
Riskoder <g.vidal1g+1@gmail.com>
2025-05-01 14:01:52 -0400

    Inicializacion de microservicio de Foros

---
+++ microservicios/microservicios-connectforo/microservicio-foros/src/main/java/com/microservicio/foros/microservicio_foros/MicroservicioForosApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.foros.microservicio_foros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioForosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioForosApplication.class, args);
+	}
+
+}
---
+++ microservicios/microservicios-connectforo/microservicio-foros/src/test/java/com/microservicio/foros/microservicio_foros/MicroservicioForosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.foros.microservicio_foros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioForosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
