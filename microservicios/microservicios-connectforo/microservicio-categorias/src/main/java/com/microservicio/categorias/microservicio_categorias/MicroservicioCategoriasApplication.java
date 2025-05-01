dab5b96755c0469ba751197705bee2adb62fab1e
Riskoder <g.vidal1g+1@gmail.com>
2025-05-01 18:48:44 -0400

    habilitar openFeign

--- microservicios/microservicios-connectforo/microservicio-categorias/src/main/java/com/microservicio/categorias/microservicio_categorias/MicroservicioCategoriasApplication.java
+++ microservicios/microservicios-connectforo/microservicio-categorias/src/main/java/com/microservicio/categorias/microservicio_categorias/MicroservicioCategoriasApplication.java
@@ -2,7 +2,8 @@ package com.microservicio.categorias.microservicio_categorias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicioCategoriasApplication {
 
