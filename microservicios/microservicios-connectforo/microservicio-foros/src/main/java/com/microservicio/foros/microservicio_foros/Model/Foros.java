c9c3accc07df71f955ba2dd214fd24eee2cec8f0
Riskoder <g.vidal1g+1@gmail.com>
2025-05-01 14:02:01 -0400

    Creacion de modelo de Foros

---
+++ microservicios/microservicios-connectforo/microservicio-foros/src/main/java/com/microservicio/foros/microservicio_foros/Model/Foros.java
@@ -0,0 +1,43 @@
+package com.microservicio.foros.microservicio_foros.Model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "foros")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Foros {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El id de la categoria no puede estar vacío")
+    @Column(name = "id_categoria", nullable = false)
+    private Long idCategoria;
+
+    @NotBlank(message = "El título no puede estar vacío")
+    @Size(max = 100, message = "El título no puede exceder los 100 caracteres")
+    @Column(nullable = false, unique = true)
+    private String titulo;
+
+    @Size(max = 500, message = "La descripción no puede exceder los 500 caracteres")
+    private String descripcion;
+
+    @Column(name = "fecha_creacion", updatable = false)
+    private LocalDateTime fechaCreacion;
+}
