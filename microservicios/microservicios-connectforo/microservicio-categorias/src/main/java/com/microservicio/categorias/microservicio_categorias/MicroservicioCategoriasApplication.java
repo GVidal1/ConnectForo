103d1a3699a9271b6f8579025640ba25272f7a23
Riskoder <g.vidal1g+1@gmail.com>
2025-05-01 20:48:42 -0400

    Habilitar el uso de OpenFeign para el consumo de microservicios externos

--- microservicios/microservicios-connectforo/microservicio-categorias/src/main/java/com/microservicio/categorias/microservicio_categorias/MicroservicioCategoriasApplication.java
+++ microservicios/microservicios-connectforo/microservicio-categorias/src/main/java/com/microservicio/categorias/microservicio_categorias/MicroservicioCategoriasApplication.java
@@ -3,6 +3,9 @@ package com.microservicio.categorias.microservicio_categorias;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicioCategoriasApplication {
