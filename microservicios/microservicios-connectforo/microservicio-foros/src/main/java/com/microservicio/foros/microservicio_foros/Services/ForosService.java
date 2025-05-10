c53721a92f5e062416f6898c75a9617c2666d8f2
Riskoder <g.vidal1g+1@gmail.com>
2025-05-10 12:34:04 -0400

    Modificacion de los metodos de servicio. Los metodos de Guardas y Actualizar Foros, ahora usan el cliente de Categorias para verificar que el idCategoria se encuentre en la base de datos.

--- microservicios/microservicios-connectforo/microservicio-foros/src/main/java/com/microservicio/foros/microservicio_foros/Services/ForosService.java
+++ microservicios/microservicios-connectforo/microservicio-foros/src/main/java/com/microservicio/foros/microservicio_foros/Services/ForosService.java
@@ -1,18 +1,23 @@
 package com.microservicio.foros.microservicio_foros.Services;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.microservicio.foros.microservicio_foros.Model.Foros;
 import com.microservicio.foros.microservicio_foros.Repository.ForosRepository;
+import com.microservicio.foros.microservicio_foros.client.CategoriaClient;
 
 @Service
 public class ForosService {
 
   @Autowired
   private ForosRepository forosRepository;
+
+  @Autowired
+  private CategoriaClient categoriaClient;
 
   public List<Foros> listarForos() {
     return forosRepository.findAll();
@@ -23,33 +28,46 @@ public class ForosService {
   }
 
   public Foros guardarForo(Foros foro) {
-    forosRepository.save(foro);
-    return foro;
+    
+    Map<String, Object> categorias = categoriaClient.obtenerCategoriaPorId(foro.getIdCategoria());
+
+    if (categorias == null || categorias.isEmpty()) {
+      throw new RuntimeException("El id de la categoria no se ha encontrado. No se puede realizar el foro.");
+    }
+
+    return forosRepository.save(foro);
+    
   }
+
   public String borrarForo(Long id) { 
-    forosRepository.deleteById(id);
+    Foros foroAct = buscarForos(id);
+    forosRepository.deleteById(foroAct.getId());
     return "Foro Eliminado correctamente";
   }
 
   public Foros actualizarForo(Long id, Foros foroActualizado) {
     Foros foroActual = buscarForos(id);
 
+    if (foroActualizado.getIdCategoria() != null) {
+        Map<String, Object> categorias = categoriaClient.obtenerCategoriaPorId(foroActualizado.getIdCategoria());
+        
+        if (categorias == null || categorias.isEmpty()) {
+            throw new RuntimeException("El id de la categoria no se ha encontrado. No se puede actualizar el foro.");
+        }
+    }
+
     if (foroActualizado.getTitulo() != null) {
-      foroActual.setTitulo(foroActualizado.getTitulo());
+        foroActual.setTitulo(foroActualizado.getTitulo());
     }
 
     if (foroActualizado.getDescripcion() != null) {
-      foroActual.setDescripcion(foroActualizado.getDescripcion());
+        foroActual.setDescripcion(foroActualizado.getDescripcion());
     }
 
     if (foroActualizado.getIdCategoria() != null) {
-      foroActual.setIdCategoria(foroActualizado.getIdCategoria());
+        foroActual.setIdCategoria(foroActualizado.getIdCategoria());
     }
 
     return forosRepository.save(foroActual);
-  }
-
-  public List<Foros> buscarForoPorIdCategoria(Long id) {
-    return forosRepository.encontrarForosPorIdCategoria(id);
-  }
+}
 }
