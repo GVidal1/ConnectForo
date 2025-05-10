cde67cd32341b4155812d558adb66a00edd5f204
Riskoder <g.vidal1g+1@gmail.com>
2025-05-09 21:11:52 -0400

    Eliminacion de anotaciones para OpenFeign

--- microservicios/microservicios-connectforo/microservicio-foros/src/main/java/com/microservicio/foros/microservicio_foros/MicroservicioForosApplication.java
+++ microservicios/microservicios-connectforo/microservicio-foros/src/main/java/com/microservicio/foros/microservicio_foros/MicroservicioForosApplication.java
@@ -2,8 +2,6 @@ package com.microservicio.foros.microservicio_foros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicioForosApplication {
 
