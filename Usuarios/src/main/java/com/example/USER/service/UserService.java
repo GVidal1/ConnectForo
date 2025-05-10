b895b7ed1dff016bf6f4c7aee6da11d592597e6f
Franar22 <fran.argandona@duocuc.cl>
2025-05-10 12:29:50 -0400

    Finalizacion de Service Usuario

--- Usuarios/src/main/java/com/example/USER/service/UserService.java
+++ Usuarios/src/main/java/com/example/USER/service/UserService.java
@@ -1,5 +1,35 @@
 package com.example.USER.service;
 
+@Service
 public class UserService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario buscarUsuarioPorId(Long id){
+        return usuarioRepository.findById(id).orElseThrow(()-> new RuntimeException("El usuario no ha sido encontrado"));
+
+    }
+
+
+    public Usuario crearUsuario(String user, String pass) {
+        
+        Usuario user1 = new Usuario();
+        user1.setUserName(user);
+        user1.setPassword(pass);
+    
+
+        return usuarioRepository.save(user1);
+    }
+
+
+
+
+
+
 
 }
