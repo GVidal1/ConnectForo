f44c182a11c0fa16a1a1575fcec847adfd590eb1
Franar22 <fran.argandona@duocuc.cl>
2025-05-10 12:05:43 -0400

    Finalizacion del model del usuario

--- Usuarios/src/main/java/com/example/USER/model/UsuarioModel.java
+++ Usuarios/src/main/java/com/example/USER/model/UsuarioModel.java
@@ -25,7 +25,7 @@ public class UsuarioModel {
     @Column(nullable = false,length = 40 )
     private String password;
 
-    @Column
+    @Column(name = "Correo Electronico", nullable = false, unique = true , length = 100)
     private String correo;
 
 
