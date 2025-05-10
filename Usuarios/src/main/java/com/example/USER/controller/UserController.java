a14fb19f7a048745d609f0aa90132c755c3099dd
Franar22 <fran.argandona@duocuc.cl>
2025-05-10 15:31:33 -0400

    Actualizacion en model,controller y service

--- Usuarios/src/main/java/com/example/USER/controller/UserController.java
+++ Usuarios/src/main/java/com/example/USER/controller/UserController.java
@@ -1,32 +1,47 @@
 package com.example.USER.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.USER.model.UsuarioModel;
+import com.example.USER.service.UserService;
+
 @RestController
 @RequestMapping("api/v1")
 public class UserController {
     
-    @Autowired
-    private UserService UserService;
+@Autowired
+private UserService userService;
 
-    @GetMapping("/users")
-    public ResponseEntity<List<Usuario>> listaDeUsuarios() {
-        List<Usuario> usuarios = usuariosService.listarUsuarios();
+@GetMapping("/users")
+public ResponseEntity<List<UsuarioModel>> listaDeUsuarios() {
+    List<UsuarioModel> usuarios = userService.listarUsuarios();
 
-        if(usuarios.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-
-        return ResponseEntity.ok(usuarios);
+    if (usuarios.isEmpty()) {
+        return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/users")
-    public ResponseEntity<?> crearUsuario(@RequestParam String userName, @RequestParam String password, @RequestParam String correo) {
-        try {
-            Usuario newUser = usuariosService.crearUsuario(userName, password, correo);
-            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e);
-        }
+    return ResponseEntity.ok(usuarios);
+}
+
+@PostMapping("/users")
+public ResponseEntity<?> crearUsuario(@RequestParam String userName, @RequestParam String password, @RequestParam String correo) {
+    try {
+        UsuarioModel newUser = userService.crearUsuario(userName, password, correo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    } catch (RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
+}
+
 
 
 
--- Usuarios/src/main/java/com/example/USER/service/UserService.java
+++ Usuarios/src/main/java/com/example/USER/service/UserService.java
@@ -1,31 +1,31 @@
 package com.example.USER.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.USER.model.UsuarioModel;
+import com.example.USER.repository.UsuarioRepository;
+
 @Service
 public class UserService {
 
-    @Autowired
+@Autowired
     private UsuarioRepository usuarioRepository;
 
-    public List<Usuario> listarUsuarios(){
-        return usuarioRepository.findAll();
-    }
+public List<UsuarioModel> listarUsuarios() {
+    return usuarioRepository.findAll();
+}
 
-    public Usuario buscarUsuarioPorId(Long id){
-        return usuarioRepository.findById(id).orElseThrow(()-> new RuntimeException("El usuario no ha sido encontrado"));
+public UsuarioModel crearUsuario(String user, String pass, String corr) {
+    UsuarioModel user1 = new UsuarioModel();
+    user1.setUserName(user);
+    user1.setPassword(pass);
+    user1.setCorreo(corr);
+    return usuarioRepository.save(user1);
+}
 
-    }
-
-
-    public Usuario crearUsuario(String user, String pass, String corr) {
-        
-        Usuario user1 = new Usuario();
-        user1.setUserName(user);
-        user1.setPassword(pass);
-        user1.setCorreo(corr);
-    
-
-        return usuarioRepository.save(user1);
-    }
 
 
 
