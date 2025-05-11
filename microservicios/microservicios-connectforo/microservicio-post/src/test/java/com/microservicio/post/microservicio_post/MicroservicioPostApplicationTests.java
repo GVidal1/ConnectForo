b43ccea96ddd1a4c90c0b0390ef57089bea65d38
Riskoder <g.vidal1g+1@gmail.com>
2025-05-10 21:13:44 -0400

    Se creo el microservicio de POST para los FOROS

---
+++ microservicios/microservicios-connectforo/microservicio-post/src/test/java/com/microservicio/post/microservicio_post/MicroservicioPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.post.microservicio_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
