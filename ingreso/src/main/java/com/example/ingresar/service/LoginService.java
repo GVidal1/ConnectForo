3e842096ca6677b5f118228986756ecdcb59218b
Franar22 <fran.argandona@duocuc.cl>
2025-05-12 18:36:12 -0400

    Modificacion de Service

--- ingreso/src/main/java/com/example/ingresar/service/LoginService.java
+++ ingreso/src/main/java/com/example/ingresar/service/LoginService.java
@@ -1,10 +1,19 @@
 package com.example.ingresar.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ingresar.model.LoginModel;
+import com.example.ingresar.repository.LoginRepository;
 
 
 @Service
 public class LoginService {
 
+
+    @Autowired
     private LoginRepository loginRepository;
 
     public LoginModel obtenerloginPorId(long idlogin){
