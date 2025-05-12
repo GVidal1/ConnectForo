9751c30528854e753344d42f135c91aa22298247
Riskoder <g.vidal1g+1@gmail.com>
2025-05-12 15:18:03 -0400

    Inicializacion de microservicio Comentarios

---
+++ microservicios/microservicios-connectforo/microservicio-comentarios/src/test/java/com/microservicio/comentarios/microservicio_comentarios/MicroservicioComentariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.comentarios.microservicio_comentarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioComentariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
