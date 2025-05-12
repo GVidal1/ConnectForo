aca7b7691d74b9d525820b0a4f004339016aad97
Franar22 <fran.argandona@duocuc.cl>
2025-05-12 18:44:56 -0400

    Modificacion controller del login

--- ingreso/src/main/java/com/example/ingresar/controller/LoginController.java
+++ ingreso/src/main/java/com/example/ingresar/controller/LoginController.java
@@ -1,4 +1,27 @@
 
+
+
+import com.example.ingresar.model.LoginModel;
+import com.example.ingresar.repository.LoginRepository;
+
+
+@RestController
+@RequestMapping("/api/login")
 public class LoginController {
+    
+    @Autowired
+    private LoginService loginService;
+
+
+    @GetMapping("/{idlogin}")
+    public ResponseEntity<?> buscarloginporid(@PathVariable Long idlogin) {
+        try {
+            LoginModel loginModel = loginService.obtenerloginPorId(idUsuario);
+            return ResponseEntity.ok(LoginModel);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
 
 }
