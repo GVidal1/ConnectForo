b0fa21e0c70ea44d6968ad097200c82c505f766e
fffffe <francisco.@fmail.com>
2025-05-14 14:30:20 -0400

    creacion de clienr nen ingreso

--- Registro/src/main/java/com/example/Registro/clients/UsuarioClient.java
+++ Registro/src/main/java/com/example/Registro/clients/UsuarioClient.java
@@ -1,5 +1,28 @@
 package com.example.Registro.clients;
 
-public class UsuarioClients {
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class UsuarioClient {
+    
+  private final WebClient webClient;
+
+  
+  public UsuarioClient(@Value("${usuarios-service.url}") String usuariosClientUrl) {
+      this.webClient = WebClient.builder().baseUrl(usuariosClientUrl).build();
+  }
+
+  
+  @SuppressWarnings("unchecked")
+  public Map<String, Object> obtenerUsuarioPorId(Long id) {
+      return this.webClient.get()
+          .uri("/{id}", id)
+          .retrieve()
+          .onStatus(status -> status.is4xxClientError(), response -> response.bodyToMono(String.class).map(body -> new RuntimeException("Usuario no encontrado")))
+          .bodyToMono(Map.class).block();
+          }
 }
