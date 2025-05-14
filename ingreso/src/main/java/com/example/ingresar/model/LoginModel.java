87323261a797a1f237ea9a6e710224522b44c2f9
fffffe <francisco.@fmail.com>
2025-05-14 13:57:52 -0400

    Creacion de Repositorio de rol

--- ingreso/src/main/java/com/example/ingresar/model/LoginModel.java
+++ ingreso/src/main/java/com/example/ingresar/model/LoginModel.java
@@ -3,14 +3,19 @@ package com.example.ingresar.model;
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Table(name = "login")
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Data
+@Entity
 public class LoginModel {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
---
+++ microservicio-rol/src/main/java/com/microservicio/rol/microservicio_rol/repository/RolRepository.java
@@ -0,0 +1,11 @@
+package com.microservicio.rol.microservicio_rol.repository;
+
+import com.microservicio.rol.microservicio_rol.model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol,Long> {
+
+}
