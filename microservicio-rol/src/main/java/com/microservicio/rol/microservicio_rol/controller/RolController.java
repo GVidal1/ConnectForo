c3c4b0f2c38d90aa4a09a3dcfbb590bb6f935516
Franar22 <fran.argandona@duocuc.cl>
2025-05-19 08:32:00 -0400

    Creacion de controller rol

--- microservicio-rol/src/main/java/com/microservicio/rol/microservicio_rol/controller/RolController.java
+++ microservicio-rol/src/main/java/com/microservicio/rol/microservicio_rol/controller/RolController.java
@@ -1,5 +1,69 @@
 package com.microservicio.rol.microservicio_rol.controller;
 
+import com.microservicio.rol.microservicio_rol.model.Rol;
+import com.microservicio.rol.microservicio_rol.service.RolService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/roles")
 public class RolController {
 
+    @Autowired
+    private RolService rolService;
+
+    @GetMapping
+    public ResponseEntity<List<Rol>> listarRoles() {
+        List<Rol> roles = rolService.listarRoles();
+        if (roles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(roles);
+    }
+
+    @GetMapping("/{idRol}")
+    public ResponseEntity<?> obtenerRolPorId(@PathVariable Long idRol) {
+        try {
+            Rol rol = rolService.obtenerRolPorId(idRol);
+            return ResponseEntity.ok(rol);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crearRol(@RequestBody @Valid Rol nuevoRol) {
+        try {
+            Rol rolGuardado = rolService.guardarRol(nuevoRol);
+            return ResponseEntity.status(HttpStatus.CREATED).body(rolGuardado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Error al crear el rol: " + e.getMessage());
+        }
+    }
+
+    @PutMapping("/{idRol}")
+    public ResponseEntity<?> actualizarRol(@PathVariable Long idRol, @RequestBody @Valid Rol nuevaInfo) {
+        try {
+            Rol rolActualizado = rolService.actualizarRol(idRol, nuevaInfo);
+            return ResponseEntity.ok(rolActualizado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Error al actualizar el rol: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{idRol}")
+    public ResponseEntity<?> borrarRol(@PathVariable Long idRol) {
+        try {
+            String mensaje = rolService.borrarRol(idRol);
+            return ResponseEntity.ok(mensaje);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
+
