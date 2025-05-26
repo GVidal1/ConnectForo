2795889ca131455afb64cb1e33cbc75502a5db44
Riskoder <g.vidal1g+1@gmail.com>
2025-05-26 13:05:32 -0400

    Agregado idRol

--- Registro/src/main/java/com/example/Registro/dto/UsuarioDTO.java
+++ Registro/src/main/java/com/example/Registro/dto/UsuarioDTO.java
@@ -5,12 +5,14 @@ import java.time.LocalDateTime;
 
 @Data
 public class UsuarioDTO {
+    private Long idRol;
     private String nombreUsuario;
     private String password;
     private String correo;
     private LocalDateTime fechaCreacion;
 
     public UsuarioDTO(String nombreUsuario, String password, String correo) {
+        this.idRol = 5L;
         this.nombreUsuario = nombreUsuario;
         this.password = password;
         this.correo = correo;
