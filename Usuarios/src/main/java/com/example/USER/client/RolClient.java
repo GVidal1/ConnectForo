99e48be4977958d0769aa3cbffb08b739c89d222
Riskoder <g.vidal1g+1@gmail.com>
2025-05-26 11:45:16 -0400

    Se agtego el cliente Rol a mic. usuarios

---
+++ Usuarios/src/main/java/com/example/USER/client/RolClient.java
@@ -0,0 +1,25 @@
+package com.example.USER.client;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class RolClient {
+  private final WebClient webClient;
+
+  public RolClient(@Value("${rol-service.url}") String rolServiceUrl) {
+      this.webClient = WebClient.builder().baseUrl(rolServiceUrl).build();
+  }
+
+  @SuppressWarnings("unchecked")
+  public Map<String, Object> obtenerRolPorId(Long id) {
+    return this.webClient.get()
+        .uri("/{idRol}", id)
+        .retrieve()
+        .onStatus(status -> status.is4xxClientError(), response -> response.bodyToMono(String.class).map(body -> new RuntimeException("Rol no encontrado")))
+        .bodyToMono(Map.class).block();
+    }
+}
