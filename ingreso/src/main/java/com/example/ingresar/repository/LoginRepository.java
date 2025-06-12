336d6c04cce3e71e7f8df99ee8dae7d66edc2f32
Riskoder <g.vidal1g+1@gmail.com>
2025-06-12 10:28:49 -0400

    Me quitaron Elementos no necesarios

--- ingreso/src/main/java/com/example/ingresar/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ingresar.repository;
-
-import com.example.ingresar.model.LoginModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginRepository extends JpaRepository<LoginModel, Long> {
-}
